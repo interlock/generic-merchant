cb2995204c4519f4179569e2f3eebe2195234f1b
James Sapara <james.sapara@gmail.com>
2014-02-15 23:15:36 -0800

    added some data about various cards to support

---
+++ test/card_companies.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var assert = require('chai').assert;
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+
+require('chai').use(sinonChai);
+
+var CardCompanies = require('../lib/card_companies');
+
+describe('CardCompanies', function () {
+  return;
+});
